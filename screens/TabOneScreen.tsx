0c96ec8e8ee2b94dfeadfc369c7e6eb133f473b9
shujinarazaki <shujinarazaki@protonmail.com>
2020-09-01 15:23:40 +0900

    add button: https://reactnative.dev/docs/button.html
    	modified:   screens/TabOneScreen.tsx

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { Button, StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
@@ -8,6 +8,11 @@ export default function TabOneScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One のタブ1</Text>
+      <Button
+       title="Learn More"
+       color="#841584"
+       accessibilityLabel="Learn more about this purple button"
+      />
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <Text style={styles.title}>Tab One 本体</Text>
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
